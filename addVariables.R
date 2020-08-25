3f1e88ddec7c142d73c8bca9b23087cd865ae36b
Vincent van Hees <vincentvanhees@gmail.com>
2020-08-25 18:44:17 +0200

    scripts moved to functions to make it easier to run the entire pipeline

--- addVariables.R
+++ addVariables.R
@@ -1,44 +1,45 @@
-rm(list=ls())
-#==========================================================
-# Input needed:
-outputdir = "/media/vincent/DATA/actometer_nkcv/output_rawactigraph_nkcv" # specify output directory
-separator = "," # Note: replace by "\t" if you are working with tab seperated data
-
-#==========================================================
-# load data
-part2_daysummary_file = paste0(outputdir,"/results/part2_daysummary.csv")
-part2_summary_file = paste0(outputdir,"/results/part2_summary.csv")
-P2 =read.csv(part2_daysummary_file, stringsAsFactors = FALSE, sep=separator)
-
-# Change ID to numeric:
-convertID = function(idValues) {
-  if (class(idValues) == "character") {
-    idValues = as.character(sapply(idValues, FUN=function(x) unlist(strsplit(x," "))[1]))
-    idValues = as.integer(idValues)
+# rm(list=ls())
+addVariables = function(outputdir=c()) {
+  #==========================================================
+  # Input needed:
+  # outputdir = "/media/vincent/DATA/actometer_nkcv/output_rawactigraph_nkcv" # specify output directory
+  separator = "," # Note: replace by "\t" if you are working with tab seperated data
+  
+  #==========================================================
+  # load data
+  part2_daysummary_file = paste0(outputdir,"/results/part2_daysummary.csv")
+  part2_summary_file = paste0(outputdir,"/results/part2_summary.csv")
+  P2 = read.csv(part2_daysummary_file, stringsAsFactors = FALSE, sep=separator)
+  
+  # Change ID to numeric:
+  convertID = function(idValues) {
+    if (class(idValues) == "character") {
+      idValues = as.character(sapply(idValues, FUN=function(x) unlist(strsplit(x," "))[1]))
+      idValues = as.integer(idValues)
+    }
+    return(idValues)
   }
-  return(idValues)
-}
-P2$ID = convertID(P2$filename)
-
-# select valid days only
-P2 = P2[which((P2$N_valid_hours_start.endhr / P2$N_hours_start.endhr) >= 0.66),] #& P2$N_hours_start.endhr >= 6
-
-# select subset of potentially relevant variables:
-
-P2 = P2[,c("ID", "mean_BFEN_mg_start.endhr", "X.0.50._BFEN_mg_start.endhr",
-           "X.50.100._BFEN_mg_start.endhr",  "X.100.150._BFEN_mg_start.endhr",
-           "X.150.200._BFEN_mg_start.endhr")]
-
-# P2 = P2[,c("ID", "mean_ENMO_mg_start.endhr", "X.0.50._ENMO_mg_start.endhr",
-#            "X.50.100._ENMO_mg_start.endhr",  "X.100.150._ENMO_mg_start.endhr",
-#            "X.150.200._ENMO_mg_start.endhr")]
-
-colnames(P2) = c("ID","act","X1","X2","X3","X4")
-
-P2 = P2[-which(is.na(P2$act) == TRUE),]
-
-# calculate slope through intensity curve
-getgradient = function(y) {
+  P2$ID = convertID(P2$filename)
+  
+  # select valid days only
+  P2 = P2[which((P2$N_valid_hours_start.endhr / P2$N_hours_start.endhr) >= 0.66),] #& P2$N_hours_start.endhr >= 6
+  
+  # select subset of potentially relevant variables:
+  
+  P2 = P2[,c("ID", "mean_BFEN_mg_start.endhr", "X.0.50._BFEN_mg_start.endhr",
+             "X.50.100._BFEN_mg_start.endhr",  "X.100.150._BFEN_mg_start.endhr",
+             "X.150.200._BFEN_mg_start.endhr")]
+  
+  # P2 = P2[,c("ID", "mean_ENMO_mg_start.endhr", "X.0.50._ENMO_mg_start.endhr",
+  #            "X.50.100._ENMO_mg_start.endhr",  "X.100.150._ENMO_mg_start.endhr",
+  #            "X.150.200._ENMO_mg_start.endhr")]
+  
+  colnames(P2) = c("ID","act","X1","X2","X3","X4")
+  
+  P2 = P2[-which(is.na(P2$act) == TRUE),]
+  
+  # calculate slope through intensity curve
+  getgradient = function(y) {
     # y: numeric vector of time spent in bins
     y = as.vector(unlist(y))
     x = c(25,75,125,175)
@@ -56,32 +57,34 @@ getgradient = function(y) {
       }
     }
     return(data.frame(gradient=gradient,y_intercept=y_intercept))
+  }
+  # derive intensity gradient
+  intgra = apply(P2[,c("X1","X2","X3","X4")],MARGIN = 1, FUN = getgradient)
+  intgra2 = data.frame(matrix(unlist(intgra), nrow=length(intgra), byrow=T))
+  colnames(intgra2) = c("gradient", "y_intercept")
+  # add to P2
+  P2 = cbind(P2, intgra2)
+  # Calculate the 91.67th percentile of the day level variables
+  D9167 = aggregate.data.frame(P2[,"act"], by = list(P2$ID), FUN = function(x) {quantile(x,11/12, na.rm = TRUE) })
+  colnames(D9167) = c("ID","act9167")
+  
+  
+  Dmean = aggregate.data.frame(P2[,c("gradient", "y_intercept",  "X1","X2","X3","X4")], by = list(P2$ID), FUN = mean, na.rm=T)
+  colnames(Dmean) = c("ID","gradient_mean","y_intercept_mean",  "X1","X2","X3","X4")
+  D = merge(D9167, Dmean, by.all="ID")
+  
+  
+  # Add the new variables to the person level output calculated by GGIR part 2:
+  P2summary = read.csv(part2_summary_file, stringsAsFactors = FALSE, sep=separator)
+  
+  # Change ID to numeric
+  P2summary$ID2 = convertID(P2summary$filename)
+  
+  # Check whether data already has the expected variables
+  existingvars = which(colnames(P2summary) %in% c("ID","act9167","gradient_mean","y_intercept_mean", "X1","X2","X3","X4"))
+  if (length(existingvars) > 0) P2summary = P2summary[,-existingvars]
+  P2summary_updated = merge(P2summary,D,by.x="ID2",by.y="ID")
+  # Save changes
+  write.csv(P2summary_updated,file=part2_summary_file,row.names = FALSE)
+  
 }
-# derive intensity gradient
-intgra = apply(P2[,c("X1","X2","X3","X4")],MARGIN = 1, FUN = getgradient)
-intgra2 = data.frame(matrix(unlist(intgra), nrow=length(intgra), byrow=T))
-colnames(intgra2) = c("gradient", "y_intercept")
-# add to P2
-P2 = cbind(P2, intgra2)
-# Calculate the 91.67th percentile of the day level variables
-D9167 = aggregate.data.frame(P2[,"act"], by = list(P2$ID), FUN = function(x) {quantile(x,11/12, na.rm = TRUE) })
-colnames(D9167) = c("ID","act9167")
-
-
-Dmean = aggregate.data.frame(P2[,c("gradient", "y_intercept",  "X1","X2","X3","X4")], by = list(P2$ID), FUN = mean, na.rm=T)
-colnames(Dmean) = c("ID","gradient_mean","y_intercept_mean",  "X1","X2","X3","X4")
-D = merge(D9167, Dmean, by.all="ID")
-
-
-# Add the new variables to the person level output calculated by GGIR part 2:
-P2summary = read.csv(part2_summary_file, stringsAsFactors = FALSE, sep=separator)
-
-# Change ID to numeric
-P2summary$ID2 = convertID(P2summary$filename)
-
-# Check whether data already has the expected variables
-existingvars = which(colnames(P2summary) %in% c("ID","act9167","gradient_mean","y_intercept_mean", "X1","X2","X3","X4"))
-if (length(existingvars) > 0) P2summary = P2summary[,-existingvars]
-P2summary_updated = merge(P2summary,D,by.x="ID2",by.y="ID")
-# Save changes
-write.csv(P2summary_updated,file=part2_summary_file,row.names = FALSE)
--- howtoapplymodel.R
+++ howtoapplymodel.R
@@ -1,25 +1,66 @@
 rm(list=ls())
 
-# Step 1: process raw accelerometer data with runGGIR.R
+datadir = "/media/vincent/DATA/actometer_nkcv/rawactigraph_nkcv"
+outputdir = "/media/vincent/DATA/actometer_nkcv"
+activitylog ="/media/vincent/DATA/actometer_nkcv/actometer_tijden/Actometer_tijden_volledig.csv"
 
-# Step 2: apply addVariables.R
+#=========================================
 
-# Step 3: Load the resulting part2_summary.csv bestand
+# TO DO: Add QC on output from activity log, GGIR, addVariables
+# TO DO: Plost classification for most recently processed recording
+# TO DO: Make option to choose hip or wrist
 
-part2_summary_file = "~/data/output_rawactigraph/results/part2_summary.csv" #part2_daysummary.csv
+# Installeer laatste versie van code
+if ("actometer_experiments" %in% rownames(installed.packages()) == FALSE) {
+  if ("devtools" %in% rownames(installed.packages()) == FALSE) {
+    install.packages("devtools")
+  }
+  library("devtools")
+  install_github("vincentvanhees/actometer_experiments")
+}
+setwd("actometer_experiments/.")
+source("runGGIR.R")
+source("convert_diary.R")
+source("addVariables.R")
+modelfile = "final_model_hip.Rdata"
+
+# Step 1: Start processing of raw accelerometer data with GGIR
+runGGIR(datadir=datadir, outputdir = outputdir, activitylog = activitylog, mode = 1)
+
+# Step 2: In the mean time convert diary into file, and safe file
+cat("\n")
+readline(prompt="Press [enter] if you sleep diary file has been created")
+if (file.exists(activitylog) == FALSE) {
+  warning("Sleep diary file as specified does not exists.")
+}
+# Step 3: Load and convert time diary
+convert_diary(activitylog = activitylog)
+
+# Step 4: Continue processing with GGIR
+runGGIR(datadir=datadir, outputdir = outputdir, activitylog = activitylog, mode = 2)
+
+# Step 5: apply addVariables.R
+tmp = unlist(strsplit(datadir,"/|\")"))
+outputdir = paste0(outputdir,"/output_",tmp[length(tmp)])
+addVariables(outputdir=outputdir)
+
+# Step 6: Load the resulting part2_summary.csv bestand
+part2_summary_file = paste0(outputdir,"/results/part2_summary.csv")
 part2_summary = read.csv(file=part2_summary_file, sep=",")
 
 # If all went well this part2_summary object will have a column act90, gradient_mean, towards the end.
 
-# Step 4: Load the trained model
-load("~/data/final_model_hip.Rdata")
+# Step 7: Load the trained model
+load(modelfile)
 
-# Step 5: Make predictions by applying the model
+# Step 8: Make predictions by applying the model
 mypredictions = stats::predict(object=final_model_hip, newdata=part2_summary)
+
 # Note that we assumed that all data in part2_summary comes from accelerometer
 # worn on the hip, because that is what the model was trained for.
-
 # If all went well object mypredictions will have the predictions.
 
-# Step 6: Save predictions
-write.csv(mypredictions, file = "~/data/mypredictions.csv")
+# Step 9: Save predictions
+write.csv(mypredictions, file = paste0(outputdir,"/results/mypredictions.csv"))
+
+
